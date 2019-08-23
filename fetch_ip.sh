bbe6db4c23a927dbd8918d4fe8c88c6af0028216
Darrell Ross <eukota@gmail.com>
2019-08-23 10:52:53 -0700

    Simple script to retrieve external facing IP
    Logs results to Slack just in case the external IP changes so that I can still ssh in.

---
+++ fetch_ip.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+SLACK_TOKEN=$(cat /opt/iot/slack_endpoint.txt)
+RESPONSE=$(curl 'https://api.ipify.org?format=json')
+IP=$(echo "${RESPONSE}" | jq ".ip" -r)
+PAYLOAD="{\"text\":\"External IP: $IP\"}"
+echo $PAYLOAD
+COMMAND="curl -X POST -H 'Content-type: application/json' --data '${PAYLOAD}' ${SLACK_TOKEN}"
+#echo "COMMAND: ${COMMAND}"
+eval $COMMAND
