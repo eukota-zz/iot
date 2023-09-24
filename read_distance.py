1a6f51c44a9ccfefed120f3fe43a24cde3a7cff0
Darrell Ross <darrell.ross@okera.com>
2023-09-24 12:46:53 -0700

    2023: use logging across the board. Updated default output to not dangerously overwrite...

--- read_distance.py
+++ read_distance.py
@@ -4,16 +4,19 @@ import logging
 import time
 from sensor import Sensor
 
+# Parse Inputs
 parser = argparse.ArgumentParser(description='Read the distance in inches from the distance sensor.')
 parser.add_argument('--interval', type=float, help='time interval to poll the sensor in seconds', default=0.25)
 args = parser.parse_args()
 read_interval = args.interval
 
-print("Distance Read...")
-print("Interval: %5.2f" % read_interval )
+# Setup
+logging.getLogger().setLevel('DEBUG')
 
+# Begin
+logging.info("Distance Read...")
+logging.info("Interval: %5.2f" % read_interval )
 with Sensor(pin_trigger=7, pin_echo=11) as sensor:
-        logging.getLogger().setLevel('DEBUG')
         while(True):
-                print("Distance: %5.2f in" % sensor.distance_in_inches())
+                logging.info("Distance: %5.2f in" % sensor.distance_in_inches())
                 time.sleep(read_interval)
