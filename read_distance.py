379b306daf45a1ad80154ad21728d594f219f5e1
Darrell Ross <darrell.ross@okera.com>
2023-09-24 13:23:30 -0700

    2023: updated read utility to exit gracefully

--- read_distance.py
+++ read_distance.py
@@ -17,6 +17,11 @@ logging.getLogger().setLevel('DEBUG')
 logging.info("Distance Read...")
 logging.info("Interval: %5.2f" % read_interval )
 with Sensor(pin_trigger=7, pin_echo=11) as sensor:
+    try:
         while(True):
-                logging.info("Distance: %5.2f in" % sensor.distance_in_inches())
-                time.sleep(read_interval)
+            logging.info("Distance: %5.2f in" % sensor.distance_in_inches())
+            time.sleep(read_interval)
+    except KeyboardInterrupt:
+        logging.info("Exiting")
+        pass
+
